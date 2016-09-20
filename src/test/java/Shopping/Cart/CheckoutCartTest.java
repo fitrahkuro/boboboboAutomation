713068ce0260ac7b987725ce261556e597331166
fitrahkuro <fitrah_anshari@yahoo.com>
2016-09-20 11:36:55 +0700

    adding class checkoutcart

--- src/test/java/Shopping/Cart/CheckoutCartTest.java
+++ src/test/java/Shopping/Cart/CheckoutCartTest.java
@@ -1,22 +1,25 @@
 package Shopping.Cart;
 
+
 import Shopping.NonEventPage;
 import UserManagement.Auth.Homepage;
-import org.openqa.selenium.By;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by fitrah on 9/20/2016.
  */
 
 
-
-public class CheckoutCart {
+public class CheckoutCartTest {
     WebDriver driver;
     Homepage homepage;
     NonEventPage searching;
